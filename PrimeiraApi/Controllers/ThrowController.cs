2ac4de77ef54325bdb71cefc54f34d2d74676f0d
jonatasNs1 <jonatassantos152409@gmail.com>
2024-01-23 11:17:38 -0300

    Handler Error

--- PrimeiraApi/Controllers/ThrowController.cs
+++ PrimeiraApi/Controllers/ThrowController.cs
@@ -1,11 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
 
 namespace PrimeiraApi.Controllers
 {
+    [ApiExplorerSettings(IgnoreApi = true)]
     public class ThrowController : ControllerBase
     {
-        [ApiExplorerSettings(IgnoreApi = true)]
-        public IActionResult HandleError()=>{
+        [Route("/error")]
+        public IActionResult HandleError() =>
+            Problem();
 
+        [Route("/error-development")]
+        public IActionResult HandleErrorDevelopment([FromServices] IHostEnvironment hostEnvironment)
+        {
+            if (!hostEnvironment.IsDevelopment())
+            {
+                return NotFound(); 
+            }
+            var exceptionHandlerFeature = 
+                HttpContext.Features.Get<IExceptionHandlerFeature>()!;
+
+            return Problem(
+                detail: exceptionHandlerFeature.Error.StackTrace,
+                title: exceptionHandlerFeature.Error.Message);
+        }
     }
 }
